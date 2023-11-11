f5401065a4811434343f4b25e3354732716a9cba
OksanaOxana <oxi_butylova@mail.ru>
2023-11-11 22:10:13 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { data } from './data';
 
 function App() {
+
+  const [systems, setSystems] = useState(0);
+
+  const {name, image, description} = data[systems];
+
+  const previousSystem = () => {
+    setSystems ((systems => {
+      systems --;
+      if (systems<0) {
+        systems = data.length-1
+      }
+      return systems
+    }))
+  }
+
+  const nextSystem = () => {
+    setSystems ((systems=> {
+      systems++;
+      if (systems>data.length-1) {
+        systems=0
+      }
+      return systems
+    }))
+  }
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mainContainer'>
+      <div className="container">
+        <h1>Human organ systems</h1>
+      </div>
+
+      <div className="container">
+        <h3>{name}</h3>
+      </div>
+     
+      <div className="container">
+        <img src={image} alt='pic' width='200px' height="250px"  />
+      </div>
+
+      <div className="container">
+        <p>{description}</p>
+      </div>
+
+      <div className="container btn">
+        <button onClick={previousSystem} >prev</button>
+        <button onClick={nextSystem} >next</button>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,69 @@
+export const data = [
+    {
+        id: 1,
+        name: 'Skeletal system',
+        image: 'https://media.istockphoto.com/id/92883350/ru/%D1%84%D0%BE%D1%82%D0%BE/%D1%80%D0%B5%D0%BD%D1%82%D0%B3%D0%B5%D0%BD%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D1%8F-%D1%81%D0%BA%D0%B5%D0%BB%D0%B5%D1%82%D0%B0.jpg?s=612x612&w=0&k=20&c=hfQ3-90xXrCg0oxAjVVnDVcp72_ZAdd6HF4dEAmUfjs=',
+        description: 'The skeletal system works as a support structure for your body. It gives the body its shape, allows movement, makes blood cells, provides protection for organs and stores minerals. '
+    },
+
+    {
+        id: 2,
+        name: 'Muscular system',
+        image: 'https://img.freepik.com/free-photo/human-body-frontal_1048-5345.jpg?size=626&ext=jpg&ga=GA1.1.760507894.1680008824&semt=ais',
+        description: "The human muscular system is the system that allows a person to move. Muscles are also part of organs; without them they could not perform their functions."
+    
+    },
+
+    {
+        id: 3,
+        name: 'Digestive system',
+        image: 'https://img.freepik.com/free-vector/diagram-showing-internal-human-digestive-name_1308-100083.jpg?size=626&ext=jpg&ga=GA1.1.760507894.1680008824&semt=ais',
+        description: 'Your digestive name is a network of organs that help you digest and absorb nutrients from food. It includes the gastrointestinal tract and biliary system.'
+    },
+
+    {
+        id: 4,
+        name: 'Respiratory system',
+        image: 'https://img.freepik.com/free-photo/human-lungs-design_1048-1686.jpg?size=626&ext=jpg&ga=GA1.1.760507894.1680008824&semt=ais',
+        description: ' This name helps your body absorb oxygen from the air so your organs can work. It also cleans waste gases, such as carbon dioxide, from your blood. '
+    },
+
+    {
+        id: 5,
+        name: 'Nervous system',
+        image: 'https://img.freepik.com/free-vector/human-nervous-system_1308-84091.jpg?size=626&ext=jpg&ga=GA1.1.760507894.1680008824&semt=ais',
+        description: 'Your nervous system is your body’s command center. Originating from your brain, it controls your movements, thoughts and automatic responses to the world around you. It also controls other body systems and processes. '
+        
+    },
+
+    {
+        id: 6,
+        name: 'Excretory system',
+        image: 'https://img.freepik.com/free-vector/human-internal-organ-with-kidneys-bladder_1308-110426.jpg?size=626&ext=jpg&ga=GA1.1.760507894.1680008824&semt=ais',
+        description: 'The excretory system includes the processes of removing harmful waste products from the body.'
+    },
+
+    {
+        id: 7,
+        name: 'The cardiovascular system',
+        image: 'https://img.freepik.com/free-vector/human-internal-organ-with-heart_1308-108214.jpg?size=626&ext=jpg&ga=GA1.1.760507894.1680008824&semt=ais',
+        description: 'The cardiovascular system has a very important function ― getting oxygen and nutrients to your entire body and removing waste. Your cells depend on your cardiovascular system to get what they need to keep running smoothly. '
+    },
+
+    {
+        id: 8,
+        name: 'The immune system',
+        image: 'https://img.freepik.com/free-psd/3d-rendering-human-body-icon_23-2150278919.jpg?size=626&ext=jpg&ga=GA1.1.760507894.1680008824&semt=sphs',
+        description: 'The organs that make up the immune system include the bone marrow, thymus, spleen, tonsils, and appendix. Your immune system is your body’s first-line defense against invaders like germs. It helps protect you from getting sick and promotes healing when you’re unwell or injured.'
+  
+    },
+
+    {
+        id: 9,
+        name: 'Endocrine system',
+        image: 'https://img.freepik.com/free-vector/brain-hypothalamus-pituitary-gland-vector_1308-127528.jpg?size=626&ext=jpg&ga=GA1.1.760507894.1680008824&semt=sph',
+        description: "The endocrine system consists of endocrine glands: pituitary gland, thyroid, parathyroid, pancreas, adrenal glands, reproductive glands. Hormones produced and released by the glands of your body's endocrine system control almost every process in your body - from metabolism to growth and development, emotions, mood, sexual function and even sleep.",
+       
+    }
+
+]
